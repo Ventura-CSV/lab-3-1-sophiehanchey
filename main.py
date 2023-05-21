9d2b53adf6f999d00ebe1776a5c3df6a102ac4a5
Kyu Lee <kwooler@gmail.com>
2023-05-21 13:29:01 -0700

    Lab 3-1

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+    number = int(input('Enter your input: '))
+
+    """
+    Make your code here
+    """
+
+    print(f'Range is {range}')
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return range
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,15 +10,16 @@ def test_main_1():
     datastr = '25'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*less than 50[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    # res = re.search(r'[\w,\W]*less than 50[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
+    assert ret == 1, 'Range 1'
 
 
 def test_main_2():
@@ -27,15 +28,16 @@ def test_main_2():
     datastr = '118'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*greater than 100[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    # res = re.search(r'[\w,\W]*greater than 100[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
+    assert ret == 3, 'Range 3'
 
 
 def test_main_3():
@@ -44,13 +46,14 @@ def test_main_3():
     datastr = '75'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(
-        r'[\w,\W]*greater than 50[\w,\W]*less than 100[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    # res = re.search(
+    #     r'[\w,\W]*greater than 50[\w,\W]*less than 100[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
+    assert ret == 2, 'Range 2'
