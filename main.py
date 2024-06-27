949c6362769d4630e2af0a60111a771abf6533fc
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-27 14:27:43 -0700

    wrote initial code and passed pytest

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
     number = int(input('Enter your input: '))
 
-    """
-    Make your code here
-    """
+    if(number<50):
+        range = 1
+    elif(number<100):
+        range = 2
+    else:
+        range = 3
+
 
     print(f'Range is {range}')
-    ########################################
+    
     # Do not delete the return statement
-    ########################################
     return range
 
 
